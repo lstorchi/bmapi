fcf6d02d3a2af3b196e1c51484d4e04958058526
Loriano Storchi <loriano@storchi.org>
2019-02-08 14:45:15 +0100

    almost completed need to check the split flag

--- bmapi.c
+++ bmapi.c
@@ -139,15 +139,32 @@ int BM_r2ow(u32 * ptr, int output_id)
   data_addr = get_out_data_addr(output_id);
   Xil_Out32 (data_addr, value);
 
+#ifndef BM_SPLIT_CTRL_REGISTERS
   control_addr = get_out_control_addr(output_id);
   controlreg = Xil_In32(control_addr);
   controlreg |= 1U << BM_DATA_VALID;
+#else
+  control_addr = get_out_data_valid_addr(output_id);
+  controlreg = Xil_In32(control_addr);
+  controlreg = 1U;
+#endif
+
   Xil_Out32 (control_addr, controlreg);
+
+#ifdef BM_SPLIT_CTRL_REGISTERS
+  control_addr = get_out_data_recv_addr(output_id);
+#endif
 
   do
   { // is it better to read before checking the bit ? 
+
+#ifndef BM_SPLIT_CTRL_REGISTERS
     controlreg = Xil_In32(control_addr);
     bitvalue = (controlreg >> BM_DATA_RECV) & 1U;
+#else
+    bitvalue = (int) Xil_In32(control_addr);
+#endif
+
   } while (bitvalue == 0);
 
   return BMOK;
@@ -155,7 +172,7 @@ int BM_r2ow(u32 * ptr, int output_id)
 
 int BM_r2owa(u32 * ptr, int output_id)
 {
-  u32 controlreg, value, control_addr, 
+  u32 controlreg = 0, value, control_addr, 
       data_addr;
   int bitvalue;
 
@@ -168,18 +185,32 @@ int BM_r2owa(u32 * ptr, int output_id)
   if (output_id > BM_UPPER_OUT_REG_LIMIT)
     return BMERR;
 
+#ifndef BM_SPLIT_CTRL_REGISTERS
   control_addr = get_out_control_addr(output_id);
+#else
+  control_addr = get_out_data_valid_addr(output_id);
+#endif
+
   data_addr = get_out_data_addr(output_id);
 
   do
   { 
+#ifndef BM_SPLIT_CTRL_REGISTERS
     controlreg = Xil_In32(control_addr);
     bitvalue = (controlreg >> BM_DATA_VALID) & 1U;
+#else
+    bitvalue = (int) Xil_In32(control_addr);
+#endif
   } while (bitvalue == 1);
 
   Xil_Out32 (data_addr, value);
 
+#ifndef BM_SPLIT_CTRL_REGISTERSz
   controlreg |= 1U << BM_DATA_VALID;
+#else
+  controlreg = 1U;
+#endif
+
   Xil_Out32 (control_addr, controlreg);
 
   return BMOK;
