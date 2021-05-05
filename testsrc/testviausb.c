9ba17b15f57b377cae25758dbb545c148f124138
Loriano Storchi <loriano@storchi.org>
2021-05-05 15:37:54 +0200

    backup

--- testsrc/testviausb.c
+++ testsrc/testviausb.c
@@ -39,7 +39,7 @@ void to_binary(unsigned char n)
 
 }
 
-int set_interface_attribs (int fd, int speed, int parity)
+int set_interface_attribs (int fd, int speed, int blocking)
 {
   struct termios tty;
   if (tcgetattr (fd, &tty) != 0)
@@ -55,7 +55,7 @@ int set_interface_attribs (int fd, int speed, int parity)
   tty.c_lflag = 0;                // no signaling chars, no echo,
                                   // no canonical processing
   tty.c_oflag = 0;                // no remapping, no delays
-  tty.c_cc[VMIN]  = 1;            // read doesn't block
+  tty.c_cc[VMIN]  = blocking;            // read doesn't block
   tty.c_cc[VTIME] = 0;            // 0.5 seconds read timeout
   
   tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
@@ -63,7 +63,7 @@ int set_interface_attribs (int fd, int speed, int parity)
   tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
                                   // enable reading
   tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-  tty.c_cflag |= parity;
+  tty.c_cflag |= 0;
   tty.c_cflag &= ~CSTOPB;
   tty.c_cflag &= ~CRTSCTS;
   
@@ -93,8 +93,16 @@ int main(int argc, char ** argv)
   	fprintf (stderr, "error %d opening %s: %s \n", errno, portname, strerror (errno));
   	return 1;
   }
+
+  set_interface_attribs (fd, 115200, 0);
+  n = 1; 
+
+  while (n != 0) // empty the buffer socat only 
+  {
+    n = read (fd, buf, sizeof(unsigned char));
+  }
   
-  set_interface_attribs (fd, 115200, 0); 
+  set_interface_attribs (fd, 115200, 1); 
 
   fprintf(stdout,"Writing 1\n");
   num = 0x15; // 00010101
@@ -105,55 +113,39 @@ int main(int argc, char ** argv)
   if (n != 1)
     return -1;
 
-  sleep(1);
-
   fprintf(stdout,"Reading 2\n");
   n = read (fd, buf, sizeof(unsigned char));
   if (n != 1)
     return -1;
   fprintf(stdout,"  ");
   to_binary(buf[0]);
-  buf[1] = buf[0] & (~ cmdMASK);
-  if (buf[1] == num)
-  {
-    fprintf(stdout,"  OK ");
-    to_binary(buf[1]);
-  }
-
-  sleep(1);
 
   fprintf(stdout,"Writing 3\n");
-  buf[0] = (unsigned char ) (buf[0] & num);
+  buf[1] = (unsigned char ) (buf[0] & num);
   fprintf(stdout,"  ");
   to_binary(buf[0]);
-  n = write(fd, buf, (size_t) 1);
+  n = write(fd, &(buf[1]), (size_t) 1);
   if (n != 1)
     return -1;
-
-  sleep(1);
 
   fprintf(stdout,"Reading 4\n");
   n = read (fd, buf, sizeof(unsigned char));
   if (n != 1)
     return -1;
-  if (buf[0] == num)
+  if (buf[0] == buf[1])
   {
     fprintf(stdout,"  OK ");
     to_binary(buf[0]);
   }
 
-  sleep(1);
-
   fprintf(stdout,"Writing 5\n");
-  buf[0] = (unsigned char ) (0x01);
+  buf[0] = (unsigned char ) (cmdNEWVAL | (0x00));
   buf[1] = (unsigned char ) 10;
   fprintf(stdout,"  ");
   to_binary(buf[0]);
   fprintf(stdout,"  ");
   to_binary(buf[1]);
-  n = write(fd, buf, (size_t) 1);
-
-  sleep(1);
+  n = write(fd, buf, (size_t) 2);
 
   fprintf(stdout,"Reading 6\n");
   n = read (fd, buf, sizeof(unsigned char) * 2);
